d8366fcd5e7917ef1a8c4ad26f4aab76b80ab7f2
User Galina <rbkgalja@yandex.ru>
2025-12-21 17:13:10 +0300

    finish commit in this modification

--- src/main/java/ru/practicum/category/service/ParentServiceImpl.java
+++ src/main/java/ru/practicum/category/service/ParentServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.category.service;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
